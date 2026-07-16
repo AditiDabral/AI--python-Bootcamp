d479013c61da55ac8922d9f41f097426521b63db
AditiDabral <dabraladiti756@gmail.com>
2026-07-16 12:56:58 +0530

    DAY 1,2,3 learning

---
+++ day1,2.py
@@ -0,0 +1,23 @@
+
+# **********factorail of number******
+
+# num=int(input("enter a number"))
+# result=1
+# while(num>0):
+#     result*=num
+#     num-=1
+# print(result)
+
+#********** largest number in list ***********
+
+# li=[1,4,32,5]
+# int_max=0
+# for item in li:
+#     if item>=int_max:
+#         int_max=item
+# print(f"{int_max} is the largest number ")
+
+# impoting a module
+import math  # u can import whole module using 
+
+from math import sqrt # for importing  specfic function from the module 
